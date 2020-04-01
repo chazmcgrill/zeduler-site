7e10b9f3baaf4c280b412c25ff1baf8e85b9dc6c
chazmcgrill <charlietaylorcoder@gmail.com>
2020-04-01 18:47:33 +0100

    Sets up gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `zeduler`,
+    description: `Currently under development.`,
+    author: `Charlie Taylor`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `zeduler-site`,
+        short_name: `zsite`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `coral`,
       marginBottom: `1.45rem`,
     }}
   >
